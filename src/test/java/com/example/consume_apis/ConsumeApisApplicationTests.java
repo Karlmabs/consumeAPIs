41f5fe858e33e4eff3625bb349972901b1cffacf
Karlmabs <maboukarl2@gmail.com>
2021-03-11 17:14:51 +0100

    new

---
+++ src/test/java/com/example/consume_apis/ConsumeApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.consume_apis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumeApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
